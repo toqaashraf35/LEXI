5baf51b07842c59e0b1f963c413e8391bc09c1d1
toqa <toqaashraff35@gmail.com>
2026-05-06 14:01:13 +0300

    resend email verification code

--- backend/lexi/apps/users/services/email_service.py
+++ backend/lexi/apps/users/services/email_service.py
@@ -1,8 +1,12 @@
 from django.core.mail import send_mail
+from django.utils import timezone
+from datetime import timedelta
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.conf import settings
+from ..models import EmailVerification
+from .utils import generate_verification_code
 
 def send_verification_email(email, code):
 
@@ -21,3 +25,22 @@ def send_verification_email(email, code):
 
     msg.attach_alternative(html_content, "text/html")
     msg.send()
+
+def resend_verification_code(user):
+    EmailVerification.objects.filter(
+        user=user,
+        is_used=False
+    ).update(is_used=True)
+
+    code = generate_verification_code()
+
+    EmailVerification.objects.create(
+        user=user,
+        code=code,
+        expires_at=timezone.now() + timedelta(minutes=10),
+        is_used=False
+    )
+
+    send_verification_email(user.email, code)
+
+    return code
