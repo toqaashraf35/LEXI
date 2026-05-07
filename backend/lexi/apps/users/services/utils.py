2125d09786a71c3a398547025b2ebf559efc3c38
toqa <toqaashraff35@gmail.com>
2026-05-07 16:07:49 +0300

    signup with google

--- backend/lexi/apps/users/services/utils.py
+++ backend/lexi/apps/users/services/utils.py
@@ -1,4 +1,20 @@
+from django.utils import timezone
+from ..models import EmailVerification
 import random
+
+def generate_verification_code(user):
+    code = str(random.randint(100000, 999999))
+    expires_at = timezone.now() + timezone.timedelta(minutes=10)
+
+    EmailVerification.objects.create(
+        user=user,
+        code=code,
+        expires_at=expires_at
+    )
+    return code
+
+def get_first_error(errors):
+    return list(errors.values())[0][0]
 
 def generate_verification_code():
     return str(random.randint(100000, 999999))
--- backend/lexi/apps/users/urls.py
+++ backend/lexi/apps/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import SignupView, VerifyEmailView, ResendVerificationCodeView
+from .views import SignupView, VerifyEmailView, ResendVerificationCodeView, GoogleAuthView, CompleteProfileView
 
 urlpatterns = [
-    path("signup/", SignupView.as_view()),
-    path("verify-email/", VerifyEmailView.as_view()),
-    path("resend-verification-code/", ResendVerificationCodeView.as_view()),
+    path("auth/signup/", SignupView.as_view()),
+    path("auth/verify-email/", VerifyEmailView.as_view()),
+    path("auth/resend-verification-code/", ResendVerificationCodeView.as_view()),
+    path('auth/google/', GoogleAuthView.as_view()),
+    path('profile/complete/', CompleteProfileView.as_view()),
 ]
