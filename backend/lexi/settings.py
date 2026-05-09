45bfcbafe48a41ebbb18d8adc65fc8dd07df7eb2
toqa <toqaashraff35@gmail.com>
2026-05-09 18:23:57 +0300

    CRUD operations for users

--- backend/lexi/settings.py
+++ backend/lexi/settings.py
@@ -85,6 +85,11 @@ DATABASES = {
     )
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 # Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
