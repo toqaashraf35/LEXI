f62cf5629e3b3ac09f7ba84f80ced12b31d8d405
toqa <toqaashraff35@gmail.com>
2026-05-16 02:36:49 +0300

    create field model and linking it to the contract

--- backend/lexi/apps/contracts/management/commands/import_contracts.py
+++ backend/lexi/apps/contracts/management/commands/import_contracts.py
@@ -1,24 +1,50 @@
 import pandas as pd
 from django.core.management.base import BaseCommand
-from lexi.apps.contracts.models import Contract
+from lexi.apps.contracts.models import Contract, Field, ContractField
+from lexi.apps.contracts.services.placeholder_service import extract_placeholders
+from lexi.apps.contracts.services.field_normalization import normalize
 
 class Command(BaseCommand):
-    help = "Import contracts from CSV"
+    help = "Import contracts + build fields + relations"
 
     def handle(self, *args, **kwargs):
 
         df = pd.read_csv(
-            "data/contracts/contracts_cleaned_dataset_final_version.xls", 
+            "data/contracts/contracts_cleaned_dataset_final_version.xls",
             encoding="utf-8-sig"
         )
 
         for _, row in df.iterrows():
 
-            Contract.objects.create(
+            content = row["Contract_Content"]
+
+            contract = Contract.objects.create(
                 name=row["Contract_Name"],
-                content=row["Contract_Content"]
+                content=content
             )
 
-        self.stdout.write(
-            self.style.SUCCESS("Contracts imported successfully")
-        )
+            placeholders = extract_placeholders(content)
+
+            for ph in set(placeholders):
+
+                ph = normalize(ph)
+
+                if not ph:
+                    continue
+
+                field, created = Field.objects.get_or_create(
+                    label=ph,
+                    defaults={
+                        "key": None,
+                        "field_type": None
+                    }
+                )
+
+                ContractField.objects.get_or_create(
+                    contract=contract,
+                    field=field
+                )
+
+        self.stdout.write(self.style.SUCCESS(
+            "Import done: contracts + fields + relations created successfully"
+        ))
